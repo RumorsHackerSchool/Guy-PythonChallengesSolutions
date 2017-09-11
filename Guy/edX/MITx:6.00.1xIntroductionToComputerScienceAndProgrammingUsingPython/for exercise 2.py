ba839200d0f7d86b0d429e1d54b2ef56f8d3b7c7
guy.zwerdling <guy.zwerdling@gmail.com>
2017-09-11 13:00:18 +0300

    upload more edx code

---
+++ Guy/edX/MITx:6.00.1xIntroductionToComputerScienceAndProgrammingUsingPython/for exercise 2.py
@@ -0,0 +1,16 @@
+'''
+2. Convert the following code into code that uses a for loop.
+
+prints "Hello!"
+prints 10
+prints 8
+prints 6
+prints 4
+prints 2
+'''
+end = 6
+var1 = 0
+var2 = end
+for lop in range(end):
+    var2 += lop
+print (var2)

cc59dd99189c7625e516088d86e691d55f2c9212
guy.zwerdling <guy.zwerdling@gmail.com>
2017-09-11 12:30:50 +0300

    upload more edx code

---
+++ Guy/edX/MITx:6.00.1xIntroductionToComputerScienceAndProgrammingUsingPython/while exercise 2.py
@@ -0,0 +1,9 @@
+None
+
+varA = 10
+while True:
+    print("Hello!")
+    while varA !=0:
+        print(varA)
+        varA -= 2
+    break
---
+++ Guy/edX/MITx:6.00.1xIntroductionToComputerScienceAndProgrammingUsingPython/while.py
@@ -0,0 +1,5 @@
+varA = 0
+while varA != 10:
+    varA += 2
+    print(varA)
+print("Goodbye!")

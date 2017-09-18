284f40c9c891a1d50a08b7788df6ebaa8b8895f6
guy.zwerdling <guy.zwerdling@gmail.com>
2017-09-18 19:49:01 +0300

    update

--- Guy/CheckPointChallenge/Networking-tryThisOne/telnet.py
+++ Guy/CheckPointChallenge/Networking-tryThisOne/telnet.py
@@ -1,10 +1,9 @@
+import telnetlib
 
 def parse_rpn(expression):
     """ Evaluate a reverse polish notation """
     """ cresit to danishmujeeb """
-
     stack = []
-
     for val in expression.split(' '):
         if val in ['-', '+', '*', '/', '^']:
             op1 = stack.pop()
@@ -17,8 +16,20 @@ def parse_rpn(expression):
             stack.append(result)
         else:
             stack.append(int(val))
-
     return stack.pop()
 
-expression = raw_input("Please type RPM: ")
-print (parse_rpn(expression))
+checkThis = 0
+tn = telnetlib.Telnet("35.158.25.165", "10111")
+while True:
+    line = tn.read_until("\n")  # Read one line
+    try:
+        if type(int(line[0]))==int:
+            print(line)
+            print("check")
+            checkThis = line
+            print(checkThis.encode('ascii'))
+            print(type(int(checkThis.encode('ascii'))))
+            print(parse_rpn(checkThis).encode('ascii'))
+
+    except ValueError:
+        continue

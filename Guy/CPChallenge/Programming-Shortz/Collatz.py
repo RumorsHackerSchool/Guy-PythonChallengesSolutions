9803759492bed07e07afba4a3e72f724ddf16947
guy.zwerdling <guy.zwerdling@gmail.com>
2017-09-19 13:40:12 +0300

    I AM THE BESTgit add *

--- Guy/CPChallenge/Programming-Shortz/Collatz.py
+++ Guy/CPChallenge/Programming-Shortz/Collatz.py
@@ -1,14 +1,47 @@
-def collatz_sequence(x):
-    seq = [int(x)]
-
+def collatz(x):
+    seq = [x]
     if x < 1:
        return []
     while x > 1:
-       if x % 4 == 0:
+        if x == 6:
+            x=1
+        if x % 2 == 0:
          x = x / 4
-       else:
-         x = 3 * x + 1
-       seq.append(x)    # Added line
+        else:
+             x = 3 * x + 1
+        seq.append(x)    # Added line
     return seq
 
-print(collatz_sequence(5))
+def Shortz(x):
+    seq = [int(x)]
+    count = 0
+    if x < 1:
+       return []
+    while x > 1:
+        if x == 6:
+            x=1
+        count +=1
+        if x % 2 == 0:
+            x = x / 4
+        else:
+            x = 3 * x + 1
+        seq.append(x)    # Added line
+    return count
+
+def ShortzSum(x):
+    seq = x
+    if x < 1:
+       return []
+    while x > 1:
+        if x == 6:
+            x=1
+        if x % 2 == 0:
+            x = x / 4
+        else:
+            x = 3 * x + 1
+        seq += x    # Added line
+    return seq
+
+
+
+print(ShortzSum(469509101546781)*Shortz(98325112))

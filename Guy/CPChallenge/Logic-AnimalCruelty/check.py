d8d123ab9a1fcd96e5b3559765fc724c7f1b388f
guy.zwerdling <guy.zwerdling@gmail.com>
2017-09-20 09:44:02 +0300

    update

--- Guy/CPChallenge/Logic-AnimalCruelty/check.py
+++ Guy/CPChallenge/Logic-AnimalCruelty/check.py
@@ -1,3 +1,4 @@
+'''
 def func(x):
         x = x / 2
         x = xor(x, 951335252)
@@ -16,3 +17,31 @@ def func(x):
 //end of func(x)
 
 func()
+
+'''
+
+def func(x):
+    x - x/2
+    x = x ^ 951335252
+    if (x and 255) > 83:
+        x - x+ 256
+    x = x/39
+
+    for lop in range(3):
+        x = x * 33
+        x = x - 1
+        x = x / 4
+        x = x^ 89
+    return x
+
+test = open('test.txt', 'w')
+
+
+for lop in range(0, 999999999):
+    x = int(str(lop) + '002')
+    print("the x is, ", x,"and the ans in func is, ",func(x))
+    test.write("the x is, ", x,"and the ans in func is, ",func(x))
+    if func(x) == 27750:
+        print("I found the ans!!! x is " + str(x))
+        test.write("I found the ans!!! x is " + str(x))
+        break

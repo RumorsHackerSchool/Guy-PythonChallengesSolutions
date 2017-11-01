b55a55533e9d921c0c80e8757679b5e36c3b17bc
guy.zwerdling <guy.zwerdling@gmail.com>
2017-11-01 11:57:53 +0200

    update

---
+++ Guy/EHC/GuyTcontrol.sh
@@ -0,0 +1,8 @@
+echo "Welcome to GuyTcontrol - man in the middle script."
+echo "Please type the ip of your VICTIM: "
+read VICTIM
+echo "Please type the ip of your DG: "
+read DG
+
+echo "$VICTIM is the victim"
+echo "$DG is the dg"

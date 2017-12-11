f5988e81b7d52ab376839b4afb90b0bd9fcab3f1
GuyZwer <guy.zwerdling@gmail.com>
2017-12-11 15:02:31 +0200

    update

--- Guy/CPChallenge/Hide-and-Seek/hide.py
+++ Guy/CPChallenge/Hide-and-Seek/hide.py
@@ -4,9 +4,9 @@ import optparse
 
 print("RGB format to hex format")
 
-r = input(please enter R: )
-g = input(please enter G: )
-b = input(please enter B: )
+r = input("please enter R: ")
+g = input("please enter G: ")
+b = input("please enter B: ")
 
 def rgb2hex(r, g, b):
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
